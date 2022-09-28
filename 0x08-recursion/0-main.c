9206310ac7a51c17c815c9374e2fd01377931b38
Favour <favourgilead@gmail.com>
2022-09-28 23:15:48 +0100

    done

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code for main School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _puts_recursion("Betty Main");
+    return (0);
+}
